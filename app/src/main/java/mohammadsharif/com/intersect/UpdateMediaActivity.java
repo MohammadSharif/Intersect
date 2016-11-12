9f9e1d7e4f0195dd6ed30569f7b97ba8504dc125
Nishant Mysore <nishraptor@gmail.com>
2016-11-12 14:27:33 -0800

    Update Media Activity Activated, Activity_update_media xml partly finished

---
+++ app/src/main/java/mohammadsharif/com/intersect/UpdateMediaActivity.java
@@ -0,0 +1,13 @@
+package mohammadsharif.com.intersect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateMediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_media);
+    }
+}

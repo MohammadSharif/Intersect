26515699bcad2c7afb33a99a7dbc884278d33b26
Mohammad <momosharif58@gmail.com>
2016-11-12 13:40:07 -0800

    Implemented dark background

--- app/src/main/java/mohammadsharif/com/intersect/FirstFragment.java
+++ app/src/main/java/mohammadsharif/com/intersect/FirstFragment.java
@@ -2,11 +2,15 @@ package mohammadsharif.com.intersect;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -17,6 +21,19 @@ public class FirstFragment extends Fragment {
         View v = inflater.inflate(R.layout.first_frag, container, false);
         TextView tv = (TextView) v.findViewById(R.id.tvFragFirst);
         tv.setText("PLACE HOLDER");
+
+        ImageButton facebook = new ImageButton(this.getContext());
+        ImageButton twitter = new ImageButton(this.getContext());
+        ImageButton linkedin = new ImageButton(this.getContext());
+        ImageButton instagram = new ImageButton(this.getContext());
+
+        LinearLayout ll = (LinearLayout)v.findViewById(R.id.buttonLayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+//        facebook.setImageDrawable();
+
+        ll.addView(facebook, lp);
 
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getActivity().setTitle("Profile");

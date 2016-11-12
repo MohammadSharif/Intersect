301364ff371f6a53d2432607a4cae0a22da74554
Mohammad <momosharif58@gmail.com>
2016-11-12 15:22:51 -0800

    Social buttons integrated, can be modified based on accessible social urls later.

--- app/src/main/java/mohammadsharif/com/intersect/FirstFragment.java
+++ app/src/main/java/mohammadsharif/com/intersect/FirstFragment.java
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.Space;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,18 +23,36 @@ public class FirstFragment extends Fragment {
         TextView tv = (TextView) v.findViewById(R.id.tvFragFirst);
         tv.setText("PLACE HOLDER");
 
-        ImageButton facebook = new ImageButton(this.getContext());
-        ImageButton twitter = new ImageButton(this.getContext());
-        ImageButton linkedin = new ImageButton(this.getContext());
-        ImageButton instagram = new ImageButton(this.getContext());
+        ImageButton facebook = new ImageButton(this.getActivity());
+        ImageButton twitter = new ImageButton(this.getActivity());
+        ImageButton linkedin = new ImageButton(this.getActivity());
+        ImageButton instagram = new ImageButton(this.getActivity());
 
         LinearLayout ll = (LinearLayout)v.findViewById(R.id.buttonLayout);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(30, 0, 30, 0);
+
+        facebook.setBackgroundResource(R.drawable.faceb);
+
+        instagram.setBackgroundResource(R.drawable.ig);
+        twitter.setBackgroundResource(R.drawable.twitt);
+        linkedin.setBackgroundResource(R.drawable.linkedin);
 
 
-//        facebook.setImageDrawable();
 
-        ll.addView(facebook, lp);
+
+        ImageButton[] buttons = new ImageButton[]{
+                facebook,
+                instagram,
+                twitter,
+                linkedin
+        };
+
+        for(ImageButton button: buttons){
+            button.setLayoutParams(lp);
+            ll.addView(button);
+        }
+
 
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getActivity().setTitle("Profile");

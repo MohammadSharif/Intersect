068b260b50c601a294456567ebb9d501a3337a16
Jean-Luc Thumm <jthumm@ucsd.edu>
2016-11-13 07:32:00 -0800

    Implement callback upon recognition

--- app/src/main/java/mohammadsharif/com/intersect/SecondFragment.java
+++ app/src/main/java/mohammadsharif/com/intersect/SecondFragment.java
@@ -1,8 +1,11 @@
 package mohammadsharif.com.intersect;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +13,9 @@ import android.widget.TextView;
 
 
 
-public class SecondFragment extends Fragment {
+public class SecondFragment extends Fragment implements CameraFragmentCallbacks {
+
+    public static final String TAG = "SecondFragment";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -18,11 +23,20 @@ public class SecondFragment extends Fragment {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getActivity().setTitle("QR Reader");
 
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.cam_placeholder, CameraFragment.newInstance(this));
+        ft.commit();
+
         return v;
     }
 
     public static SecondFragment newInstance() {
         SecondFragment f = new SecondFragment();
         return f;
+    }
+
+    @Override
+    public void postRecognition(String firebaseID, String formalName) {
+        Log.d(TAG, firebaseID + " | " + formalName);
     }
 }

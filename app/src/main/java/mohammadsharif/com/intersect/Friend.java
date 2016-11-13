6b80a1d47f62f235df9cbf0ac81465824cfa5ead
Arkin Gupta <arg061@ucsd.edu>
2016-11-13 07:37:43 -0800

    Added search functionality and friend list

--- app/src/main/java/mohammadsharif/com/intersect/Friend.java
+++ app/src/main/java/mohammadsharif/com/intersect/Friend.java
@@ -6,12 +6,17 @@ import java.sql.Time;
 
 public class Friend extends User {
 
+    ThirdFragment ref_3;
     private Location meetLocation;
         private String time;
+
+
     public Friend(User toBefriend, Location meetLocation, String time){
         super(toBefriend.getUserID(), toBefriend.getName(), toBefriend.getFriends());
         this.meetLocation = meetLocation;
         this.time = time;
+        //Adding to visible friend list
+        ref_3.addFriend(toBefriend);
     }
 
     public Location getMeetLocation() {
--- app/src/main/java/mohammadsharif/com/intersect/ThirdFragment.java
+++ app/src/main/java/mohammadsharif/com/intersect/ThirdFragment.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,14 +27,34 @@ import java.util.List;
 public class ThirdFragment extends Fragment {
 
     private RecyclerView rv;
+    private EditText search;
+    private Button searchButton;
+    private User thisGuy;
+    public ArrayList<Friend> friends;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        search = new EditText(this.getActivity());
+        searchButton = new Button(this.getActivity());
+
+        //Please crosscheck attributes on this one
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                String name = search.getText().toString();
+                search(name); //Calling search method
+
+            }
+        });
+
+
         View v = inflater.inflate(R.layout.third_frag, container, false);
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         getActivity().setTitle("Ads");
 
-        rv = (RecyclerView)v.findViewById(R.id.rv);
+        rv = (RecyclerView) v.findViewById(R.id.rv);
 
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
         rv.setLayoutManager(llm);
@@ -43,6 +66,28 @@ public class ThirdFragment extends Fragment {
     public static ThirdFragment newInstance() {
         ThirdFragment f = new ThirdFragment();
         return f;
+
+    }
+
+    public void addFriend(User toBeFriend) {
+        System.out.println(toBeFriend.getName());
+        //thisGuy.something .? Need a little help here
+        //Display in recycler view
+    }
+
+    public void search(String name) {
+
+        friends = thisGuy.getFriends();
+
+        for (Friend Fname : friends) {
+
+            if (Fname.getName().equals(name)) {
+
+                //Display only this entry in recycler view
+
+            }
+
+        }
 
     }
 
